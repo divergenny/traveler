74134baed056b7d8f84772c12afbef4c8e19f792
divergenny <divergenny@gmail.com>
2022-05-25 21:05:42 +0500

    fix: remove unnecessary imports and methods.

--- src/main/java/by/divergenny/traveler/mapper/UserMapper.java
+++ src/main/java/by/divergenny/traveler/mapper/UserMapper.java
@@ -4,12 +4,9 @@ import by.divergenny.traveler.domain.User;
 import by.divergenny.traveler.dto.UserDto;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User entity);
+
     User toEntity(UserDto dto);
-    List<UserDto> toDtos(List<User> entities);
-    List<User> toEntities(List<UserDto> dtos);
 }
