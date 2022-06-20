754302551d4e6833e85d7509b5138be049e8a580
divergenny <divergenny@gmail.com>
2022-06-20 14:44:24 +0500

    add: Controller, Consumer, receiver, runner, producer

--- rabbit-mq/src/main/java/by/divergenny/traveler/rabbitmq/RabbitMqApplication.java
+++ rabbit-mq/src/main/java/by/divergenny/traveler/rabbitmq/RabbitMqApplication.java
@@ -1,13 +1,17 @@
 package by.divergenny.traveler.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.websocket.Endpoint;
 
 @SpringBootApplication
 public class RabbitMqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqApplication.class, args);
+
     }
 
 
