c44026144a6192add863ddf54566fbdcbe366a20
divergenny <divergenny@gmail.com>
2022-06-28 16:18:42 +0500

    add and fix http method request

---
+++ src/main/java/by/divergenny/traveler/exception/ServiceTripException.java
@@ -0,0 +1,9 @@
+package by.divergenny.traveler.exception;
+
+public class ServiceTripException extends BaseException {
+
+    public ServiceTripException(String message) {
+        super(message);
+    }
+
+}
